0f5731be02dc4e1d160d15a89f642facb3143e2c
Noelia-F <noelia.fronton.g@gmail.com>
2025-07-25 00:20:33 +0200

    Part1: exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,20 +4,51 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [points, setPoints] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positivePercentage, setPositivePercentage] = useState(0);
 
   const handleGoodClick = () => {
     const newValue = good + 1;
+    const total = all + 1;
+    const newPoints = points + 1;
+    const newAverage = newPoints / total;
+    const newPositivePercentage = (newValue / total) * 100;
+
+    setAll(total);
+    setPoints(newPoints);
+    setAverage(newAverage);
     setGood(newValue);
+    setPositivePercentage(newPositivePercentage);
   }
 
   const handleNeutralClick = () => {
     const newValue = neutral + 1;
+    const total = all + 1;
+    const newPoints = points;
+    const newAverage = newPoints / total;
+    const newPositivePercentage = (good / total) * 100;
+  
+    setAll(total);
+    setPoints(newPoints);
+    setAverage(newAverage);
     setNeutral(newValue);
+    setPositivePercentage(newPositivePercentage);
   }
 
   const handleBadClick = () => {
     const newValue = bad + 1;
+    const total = all + 1;
+    const newPoints = points - 1;
+    const newAverage = newPoints / total;
+    const newPositivePercentage = (good / total) * 100;
+
+    setAll(total);
+    setPoints(newPoints);
+    setAverage(newAverage);
     setBad(newValue);
+    setPositivePercentage(newPositivePercentage);
   }
 
   return (
@@ -31,6 +62,9 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {average}</p>
+        <p>positive {positivePercentage}%</p>
       </div>
     </div>
   )
